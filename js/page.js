a58b1d1bb1a71b29b35afb531db9f02fb3da5cc4
Raiady Valentine Soares De Franca <valentineraiady@gmail.com>
2023-08-22 16:55:14 -0400

    initial commit

---
+++ js/page.js
@@ -0,0 +1,11 @@
+const botao = document.getElementById('botao');
+  const mensagem = document.querySelector('.mensagem');
+
+  botao.addEventListener('click', () => {
+    if (mensagem.style.display === 'none' || mensagem.style.display === '') {
+      mensagem.style.display = 'block';
+    } else {
+      mensagem.style.display = 'none';
+    }
+  });
+
