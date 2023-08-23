5a94241de1c96c516beb6163af23067460ebb48c
Raiady Valentine Soares De Franca <valentineraiady@gmail.com>
2023-08-23 14:45:44 -0400

    initial commit

--- js/page.js
+++ js/page.js
@@ -9,3 +9,20 @@ const botao = document.getElementById('botao');
     }
   });
 
+  const carousel = document.querySelector('.carousel');
+  const images = document.querySelectorAll('.carousel img');
+  let currentIndex = 0;
+  const intervalTime = 2000; // Intervalo de 3 segundos
+  
+  function showNextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateCarouselPosition();
+  }
+  
+  function updateCarouselPosition() {
+    const imageWidth = images[0].width;
+    const translateX = -currentIndex * imageWidth;
+    carousel.style.transform = `translateX(${translateX}px)`;
+  }
+  
+  setInterval(showNextImage, intervalTime);
